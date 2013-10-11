67902dd6dafa15c2234849b58f18b8e06c4032b0
Doug Winter <doug.winter@isotoma.com>
2013-10-11 13:02:31 +0100

    now has an app

--- chaser/settings.py
+++ chaser/settings.py
@@ -113,10 +113,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gunicorn',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'chaser.chaser'
 )
 
 # A sample logging configuration. The only tangible logging
