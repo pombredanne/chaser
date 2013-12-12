10419ebd9c3130122faf0e0a5832c54b33ba48bf
Doug Winter <doug.winter@isotoma.com>
2013-12-12 15:57:21 +0000

    working view

--- chaser/urls.py
+++ chaser/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from .chaser import views
+
 urlpatterns = patterns('',
-    #url(r'^$', 'chaser.views.home', name='home'),
+    url(r'^$', views.index, name='index'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
