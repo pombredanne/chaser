1e4731f779bd3bf6ceca96ba0e5195dff510f2b0
Doug Winter <doug.winter@isotoma.com>
2013-12-12 16:20:39 +0000

    query the database

--- chaser/chaser/views.py
+++ chaser/chaser/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import *
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    chasers = Chaser.objects.order_by('-pub_date')[:5]
+    template = loader.get_template("chaser/index.html")
+    context =  RequestContext(request, {
+         'chasers': chasers,
+    })
+    return HttpResponse(template.render(context))
+
